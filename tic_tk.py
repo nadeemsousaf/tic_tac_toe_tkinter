a5c4886c00d257e8273f9d3e98946e4fcf6ca414
nadeemsousaf <nmsou6754@gmail.com>
2024-12-31 14:34:23 -0500

    opponent

--- tic_tk.py
+++ tic_tk.py
@@ -1,108 +1,181 @@
-from tkinter import * 
+from tkinter import *
+import random
 
 game = Tk()
 game.geometry("400x400")
 
 canvas = Canvas(game, width=400, height=400)
+
 font1 = ("Arial", 40, "bold")
+font2 = ("Arial", 20, "bold")
 
-player_move = 'x'
-empty = ' '
-
+empty = "    "
+user = " X "
+opponent = " O "
 
 board = []
+taken_positions = 0
+
 
 class square:
-    def __init__(self, type, x_num, y_num):
-        self.type = type
+    def __init__(self, button, x_num, y_num, type):
+        self.button = button
         self.x_num = x_num
         self.y_num = y_num
+        self.type = type
         
 def create_board():
-    #global board
-    board.append(square(empty,600,250))
-    board.append(square(empty,800,250))
-    board.append(square(empty,1000,250))
-    board.append(square(empty,600,450))
-    board.append(square(empty,800,450))
-    board.append(square(empty,1000,450))
-    board.append(square(empty,600,650))
-    board.append(square(empty,800,650))
-    board.append(square(empty,1000,650))
-
-def update_board():
-    for i in board:
-        if i.type != empty:
-            if i.type == 'X':
-                canvas.create_window(i.x_num, i.y_num, window = button_x)
-            else:
-                canvas.create_window(i.x_num, i.y_num, window = button_o)
+    board.append(square(button_r1_c1,600,250,empty))
+    board.append(square(button_r1_c2,800,250,empty))
+    board.append(square(button_r1_c3,1000,250,empty))
+    board.append(square(button_r2_c1,600,450,empty))
+    board.append(square(button_r2_c2,800,450,empty))
+    board.append(square(button_r2_c3,1000,450,empty))
+    board.append(square(button_r3_c1,600,650,empty))
+    board.append(square(button_r3_c2,800,650,empty))
+    board.append(square(button_r3_c3,1000,650,empty))
 
 
-def opponent_move():
-    global player_move
-    player_move = 'O'
-
-def user_move():
-    global player_move
-    player_move = 'X'
+def new_move():
+    global player
+    if player == user:
+        player = opponent
+        opponent_play()
+    else:
+        player = user
 
 def opponent_play(): #generate opponent move
-    ok = 1
+    found_move = False
+    while found_move == False:
+        position = random.randrange(0,8)
+        if board[position].type == empty:
+            found_move = True #we have found a valid move
+            if position == 0:
+                r1_c1()
+            elif position == 1:
+                r1_c2()
+            elif position == 2:
+                r1_c3()
+            elif position == 3:
+                r2_c1()
+            elif position == 4:
+                r2_c2()
+            elif position == 5:
+                r2_c3()
+            elif position == 6:
+                r3_c1()
+            elif position == 7:
+                r3_c2()
+            else:
+                r3_c3()
 
 def start():
     create_board()
+    load_board()
+
+def load_board():
     canvas.pack(fill="both", expand=True)
-    canvas.create_window(600, 250, window = button_r1_c1)
-    canvas.create_window(800, 250, window = button_r1_c2)
-    canvas.create_window(1000, 250, window = button_r1_c3)
-    canvas.create_window(600, 450, window = button_r2_c1)
-    canvas.create_window(800, 450, window = button_r2_c2)
-    canvas.create_window(1000, 450, window = button_r2_c3)
-    canvas.create_window(600, 650, window = button_r3_c1)
-    canvas.create_window(800, 650, window = button_r3_c2)
-    canvas.create_window(1000, 650, window = button_r3_c3)
+    for i in board:
+        canvas.create_window(i.x_num, i.y_num, window = i.button)
+    canvas.create_window(1425, 55, window = button_quit)
+
+def formal_end_game(message):
+    canvas.pack_forget()
+    canvas.create_window(800, 450, window = button_quit)
+    canvas.create_text(800, 300, text = message)
+
+def valid_move(button, position):
+    global taken_positions
+    button.configure(text=player)
+    board[position].type = player
+    taken_positions += 1
+    check_game_status()
+    new_move()
 
 def r1_c1():
-    board[0].type = player_move
+    if(button_r1_c1["text"]==empty):
+        valid_move(button_r1_c1, 0)
 
 def r1_c2():
-    board[1].type = player_move
+    if(button_r1_c2["text"]==empty):
+        valid_move(button_r1_c2, 1)
 
 def r1_c3():
-    board[2].type = player_move
+    if(button_r1_c3["text"]==empty):
+        valid_move(button_r1_c3, 2)
 
 def r2_c1():
-    board[3].type = player_move
+    if(button_r2_c1["text"]==empty):
+        valid_move(button_r2_c1, 3)
 
 def r2_c2():
-    board[4].type = player_move
+    if(button_r2_c2["text"]==empty):
+        valid_move(button_r2_c2, 4)
 
 def r2_c3():
-    board[5].type = player_move
+    if(button_r2_c3["text"]==empty):
+        valid_move(button_r2_c3, 5)
 
 def r3_c1():
-    board[6].type = player_move
+    if(button_r3_c1["text"]==empty):
+        valid_move(button_r3_c1, 6)
 
 def r3_c2():
-    board[7].type = player_move
+    if(button_r3_c2["text"]==empty):
+        valid_move(button_r3_c2, 7)
 
 def r3_c3():
-    board[8].type = player_move
+    if(button_r3_c3["text"]==empty):
+        valid_move(button_r3_c3, 8)
+
+
+def check_game_status():
+    player_win = False
+    player_tie = False
+    if board[0].type == player and board[1].type == player and board[2].type == player:
+        player_win = player
+    if board[3].type == player and board[4].type == player and board[5].type == player:
+        player_win = player
+    if board[6].type == player and board[7].type == player and board[8].type == player:
+        player_win = player
+    if board[0].type == player and board[3].type == player and board[6].type == player:
+        player_win = player
+    if board[1].type == player and board[4].type == player and board[7].type == player:
+        player_win = player
+    if board[2].type == player and board[5].type == player and board[8].type == player:
+        player_win = player
+    if board[0].type == player and board[4].type == player and board[8].type == player:
+        player_win = player
+    if board[2].type == player and board[4].type == player and board[6].type == player:
+        player_win = player
+    if taken_positions == 9:
+        player_tie = True
+    create_message(player_win, player_tie)
+
+
+def create_message(player_win, player_tie):
+    if player_win != False:
+        if player_win == user:
+            formal_end_game("Congratulations, you won!")
+        else:
+            formal_end_game("Sorry, you lost!")
+    else:
+        if player_tie == True:
+            formal_end_game("We have a tie!")
 
 #game buttons
-button_r1_c1 = Button(game, text = "   ", font = font1, command=r1_c1)
-button_r1_c2 = Button(game, text = "   ", font = font1, command=r1_c2)
-button_r1_c3 = Button(game, text = "   ", font = font1, command=r1_c3)
-button_r2_c1 = Button(game, text = "   ", font = font1, command=r2_c1)
-button_r2_c2 = Button(game, text = "   ", font = font1, command=r2_c2)
-button_r2_c3 = Button(game, text = "   ", font = font1, command=r2_c3)
-button_r3_c1 = Button(game, text = "   ", font = font1, command=r3_c1)
-button_r3_c2 = Button(game, text = "   ", font = font1, command=r3_c2)
-button_r3_c3 = Button(game, text = "   ", font = font1, command=r3_c3)
-button_x = Button(game, text = " X ", font = font1)
-button_o = Button(game, text = " O ", font = font1)
-button_quit = Button(game, text = "Quit Game", font = font1, command=game.destroy)
+button_r1_c1 = Button(game, text = empty, font = font1, command=r1_c1)
+button_r1_c2 = Button(game, text = empty, font = font1, command=r1_c2)
+button_r1_c3 = Button(game, text = empty, font = font1, command=r1_c3)
+button_r2_c1 = Button(game, text = empty, font = font1, command=r2_c1)
+button_r2_c2 = Button(game, text = empty, font = font1, command=r2_c2)
+button_r2_c3 = Button(game, text = empty, font = font1, command=r2_c3)
+button_r3_c1 = Button(game, text = empty, font = font1, command=r3_c1)
+button_r3_c2 = Button(game, text = empty, font = font1, command=r3_c2)
+button_r3_c3 = Button(game, text = empty, font = font1, command=r3_c3)
+button_quit = Button(game, text = "Quit Game", font = font2, command=game.destroy)
+
+player = user #setting current player turn
 
 start()
 
